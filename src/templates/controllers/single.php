68a9f0795448e8eccef066dcb365ad71a329f4b1
Joshua Seigler <github@joshua.seigler.net>
2017-07-12 01:21:21 -0400

    add sidebar of recent posts, add post breadcrumbs

--- src/templates/controllers/single.php
+++ src/templates/controllers/single.php
@@ -2,4 +2,10 @@
 
 $data = \Timber\Timber::get_context();
 $data['post'] = new \Timber\Post();
+$query = new WP_Query(array(
+  'numberposts' => 5,
+  'orderby' => 'date',
+  'post__not_in' => array($data['post']->id)
+));
+$data['posts'] = new \Timber\PostQuery($query);
 \Timber\Timber::render('single.twig', $data);
