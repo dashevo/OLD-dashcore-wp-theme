0b7a697abb04ee500b0e22259e80863c53d9c678
Joshua Seigler <github@joshua.seigler.net>
2017-07-18 19:22:26 -0400

    add (unstyled) language links to header

--- src/templates/controllers/index.php
+++ src/templates/controllers/index.php
@@ -1,4 +1,10 @@
 <?php
 
 $data = \Timber\Timber::get_context();
+$data['pll__'] = new \Timber\FunctionWrapper('pll__');
+$data['pll_the_languages'] = pll_the_languages(array(
+  'raw'=>1,
+  'hide_if_empty'=>0,
+  'force_home'=>1
+));
 \Timber\Timber::render('index.twig', $data);
